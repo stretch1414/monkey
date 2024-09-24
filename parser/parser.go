eb61bb06315f9e4903406f4699735221d4b901d4
stretch1414 <jmoser1414@gmail.com>
2024-09-23 22:19:50 -0400

    2.5 - Parsing return statements

--- parser/parser.go
+++ parser/parser.go
@@ -79,6 +79,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -96,6 +98,20 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
+
+	// TODO: We're skipping expressions until we
+	// encounter a semicolon.
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
 
 	// TODO: We're skipping expressions until we
 	// encounter a semicolon.
